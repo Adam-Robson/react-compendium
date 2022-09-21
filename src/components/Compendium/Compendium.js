b02230870d251f3356142af3165df0c8361a7544
Adam Robson <adamray312@gmail.com>
2022-09-21 16:49:14 -0700

    format the name and descriptions of each character

--- src/components/Compendium/Compendium.js
+++ src/components/Compendium/Compendium.js
@@ -11,9 +11,19 @@ export default function Compendium() {
       <Select options={types} changeHandler={setSelectedType} />
       {pokemon.map((poke) => (
         <div key={poke.id}>
-          <h1>{poke.pokemon}</h1>
-          <h3>{poke.type_1}</h3>
-          <h3>{poke.type_2}</h3>
+          <div className="container">
+            <img src={poke.url_image} />
+            <div className="middle">
+              <div className="text">
+                <h1>{poke.pokemon}</h1>
+                <h3>type: {poke.type_1}</h3>
+                <h3>health: {poke.hp}</h3>
+                <h3>attack: {poke.attack}</h3>
+                <h3>defense: {poke.defense}</h3>
+                <h3>speed: {poke.speed}</h3>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
